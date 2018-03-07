45baa1510968d64eeac617c9a46d0c6744936b29
Craig Pestell <craigpestell@gmail.com>
2018-03-06 20:40:55 -0800

    oauth2callback route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get(['/','/oauth2callback'], function(req, res, next) {
   require('../quickstart');
   res.render('index', { title: 'Express' });
 });
